759fad122d16fe77cfcb0c243cd51403b0474bad
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2025-05-10 06:31:28 -0300

    Concluída a Atividade 2

---
+++ Atividade2/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2
+{
+    public static class Fibonacci
+    {
+        public static bool PertenceFibonacci(int numero)
+        {
+            if (numero < 0)
+                return false;
+
+            int a = 0, b = 1;
+
+            while (b < numero)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return numero == a || numero == b;
+        }
+    }
+}
---
+++ Atividade2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Atividade2;
+
+Console.Write("Informe um número: ");
+
+if (!int.TryParse(Console.ReadLine(), out int numero))
+{
+    Console.WriteLine("Entrada inválida. Por favor, apenas números.");
+    return;
+}
+
+if (!Fibonacci.PertenceFibonacci(numero))
+{
+    Console.WriteLine($"{numero} Não pertence à sequência de Fibonacci.");
+    return;
+}
+
+Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
