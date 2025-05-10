eb5fca0d15504e4f932293e74f3417d994f4781e
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2025-05-10 06:34:13 -0300

    Concluída a Atividade 5

---
+++ Atividade5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Escreva uma palavra para ser invertida.");
+string original = Console.ReadLine()!;
+
+string invertida = string.Empty;
+for (int i = original.Length - 1; i >= 0; i--)
+{
+    invertida += original[i];
+}
+
+Console.WriteLine($"String original: {original}");
+Console.WriteLine($"String invertida: {invertida}");
